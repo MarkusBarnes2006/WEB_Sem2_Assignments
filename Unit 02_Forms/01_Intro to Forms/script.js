6a461285b1c01d04db2fe4bcbb22964d041ba946
Mark Barnes <barnemar002@warren.k12.in.us>
2024-01-19 09:04:06 -0500

    2.2

--- Unit 02_Forms/01_Intro to Forms/script.js
+++ Unit 02_Forms/01_Intro to Forms/script.js
@@ -1,6 +1,6 @@
 function validateForm(){
     let x = document.forms["myForm"]["fname"].value;
-    if(x == ""){
+    if(x == "");
         alert("Name must be filled out");
         return false;
     }
