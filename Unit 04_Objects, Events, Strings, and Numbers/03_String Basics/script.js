5e7452d801060323324eeb8694e205844e0fd8fb
Mark Barnes <barnemar002@warren.k12.in.us>
2024-02-02 08:58:05 -0500

    4.3

---
+++ Unit 04_Objects, Events, Strings, and Numbers/03_String Basics/script.js
@@ -0,0 +1,66 @@
+let lyrics = `
+I'd rather be with you, yeah
+Yeah, I'd rather be with you
+I'd rather be with you, yeah
+Yeah I'd rather be with you
+
+I'd rather be with you until I'm through
+Oh yes I do
+I'd rather be with you until that day we'll fly away
+I just love that smiling face in the early sun
+If I can't have you to myself, then life's no fun
+
+I'd rather be with you, yeah
+Yeah, I'd rather be with you
+I'd rather be with you, yeah
+Yeah I'd rather be with you
+
+I want to hold you hand
+Oh, if I can just be your man
+I want to be you friend
+Not now and then, but until the end
+
+I just love the way you act, and that's a fact
+I want to be your number one, so picture that
+
+Maybe the next time
+Maybe the next time
+Maybe the next time
+Maybe the next time
+
+I'd rather be with you
+Yeah
+Yeah, I'd rather be with you
+
+I'd rather be with you until I'm through
+Oh, yes I do
+I'd rather be with you until that day we'll fly away
+I just love that smiling face in the early sun
+If I can't have you to myself, then life's no fun
+
+I'd rather be with you, yeah
+Yeah, I'd rather be with you
+I'd rather be with you, yeah
+Yeah I'd rather be with you
+
+Oh babe
+Well, uh, you might think I'm trying to be funny
+But I'm really serious this time, baby
+    (I'd rather be with you, yeah)
+
+Even though, uh, it's a cold world, baby
+But you know deep down inside that I do love ya
+I know I sound strange, but I really mean it(I'd rather be with you, yeah)
+
+We gonna make it this time baby
+        (I'd rather be with you, yeah) you're the only one I really love now
+I'm coming at you with both hands tied behind my back, baby
+
+            (I'd rather be with you, yeah) you got me, I'm coming at you, get ready
+I'm gonna stick my love in your eye, baby (I'd rather be with you, yeah)
+
+You can see me coming baby, just coming all over you
+    (Yeah I'd rather be with you);
+`;
+
+document.getElementById("lyrics").innerHTML = lyrics;
