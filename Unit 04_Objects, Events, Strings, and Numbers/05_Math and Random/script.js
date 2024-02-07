6eb8e2aff6cbf68c16cfcf24114610c1702b80b0
Mark Barnes <barnemar002@warren.k12.in.us>
2024-02-07 08:42:35 -0500

    4.5.2

--- Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function takeSquareroot(){
     let number = +document.getElementById("input1").value;
     
@@ -6,15 +5,3 @@ function takeSquareroot(){
     let Squareroot = Math.sqrt(number);
     document.getElementById("result").innerHTML = Squareroot;
 }
-=======
-function takeSquareRoot() {
-    let number = +document.getElementById("input1").value;
-    
-
-    let squareRoot = Math.sqrt(number);
-    document.getElementById("result").innerHTML = squareRoot;
-    
-}
-
-// Make another function for takeAbsoluteValue
->>>>>>> 12abe48ed1a2631663919052fa3a8b8c7b7bee3f
