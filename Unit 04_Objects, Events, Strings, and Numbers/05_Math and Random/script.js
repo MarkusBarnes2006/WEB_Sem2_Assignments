62452a26291e4104ffaf25f94d241ceff7a7285a
Mark Barnes <barnemar002@warren.k12.in.us>
2024-02-08 08:45:49 -0500

    4.5.3

--- Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
@@ -5,3 +5,26 @@ function takeSquareroot(){
     let Squareroot = Math.sqrt(number);
     document.getElementById("result").innerHTML = Squareroot;
 }
+
+function takePower(){
+    let number1 = +document.getElementById("input1").value;
+    let number2 = +document.getElementById("input2").value;
+
+    let Power = Math.pow(number1, number2);
+    document.getElementById("result").innerHTML = Power;
+}
+function takeCubeRoot(){
+    let number = +document.getElementById("input1").value;
+    
+    
+    let CubeRoot = Math.cbrt(number);
+    document.getElementById("result").innerHTML = CubeRoot;
+}
+function takeExponent(){
+    let number = +document.getElementById("input1").value;
+    
+    
+    let Exponent = Math.exp(number);
+    document.getElementById("result").innerHTML = Exponent;
+}
+
