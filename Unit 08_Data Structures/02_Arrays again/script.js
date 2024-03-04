3973765765aea901bec42a57928528a44b3ca81f
Mark Barnes <barnemar002@warren.k12.in.us>
2024-03-04 08:41:02 -0500

    8.2

---
+++ Unit 08_Data Structures/02_Arrays again/script.js
@@ -0,0 +1,14 @@
+const numbers = []
+
+for(let i = 0; i < 10; i++){
+    let num = Math.floor(Math.random() * 10) + 1;
+    
+    if(num % 2 !== 0){
+        numbers.push(num);
+    }
+}
+
+document.getElementById("unsorted").innerHTML = numbers;
+
+numbers.sort(function(a, b){return a - b});
+document.getElementById("sorted").innerHTML = numbers
